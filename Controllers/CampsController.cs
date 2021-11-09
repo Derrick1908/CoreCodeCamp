98edb1ce7b866834236e32f6a0c7b12f5526b48e
Derrick Nazareth <derrick.nazareth@teknorix.com>
2021-11-09 14:23:02 +0530

    Deleted the Values Controller; Added CampsController; Added CampModel

---
+++ Controllers/CampsController.cs
@@ -0,0 +1,34 @@
+﻿using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+        public CampsController(ICampRepository repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetCamps()
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsAsync();
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+    }
+}
