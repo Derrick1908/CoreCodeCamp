4f119750de543643ae4f2443dafafecac1ab5685
Derrick Nazareth <derrick.nazareth@teknorix.com>
2021-11-10 15:13:11 +0530

    Added URL Versioning to Camps Controller; Added URL Versioning through Startup Class also.;

--- Startup.cs
+++ Startup.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using CoreCodeCamp.Controllers;
 using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.Versioning.Conventions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,12 +31,23 @@ namespace CoreCodeCamp
           opt.AssumeDefaultVersionWhenUnspecified = true;
           opt.DefaultApiVersion = new ApiVersion(1, 1);
           opt.ReportApiVersions = true;
-          //opt.ApiVersionReader = new QueryStringApiVersionReader("ver");  //Used to Change the Query String from ?api-version to ?ver
-          //opt.ApiVersionReader = new HeaderApiVersionReader("X-Version");   //Using a Header "X-Version" we can specify which Version we want rather than specifying Query String
+          opt.ApiVersionReader = new UrlSegmentApiVersionReader();
+
+          /*
+          opt.ApiVersionReader = new QueryStringApiVersionReader("ver");  //Used to Change the Query String from ?api-version to ?ver
+          opt.ApiVersionReader = new HeaderApiVersionReader("X-Version");   //Using a Header "X-Version" we can specify which Version we want rather than specifying Query String
           opt.ApiVersionReader = ApiVersionReader.Combine(              //Either Header or Query String can be used
               new HeaderApiVersionReader("X-Version"),  
               new QueryStringApiVersionReader("ver","version")          //Either Ver or Version can be used
               );
+          */
+          /*
+          opt.Conventions.Controller<TalksController>()                 //Rather than specifiying Version Information on the Controller Itself, we can place only that Info Centrally in the Start Up Class.
+             .HasApiVersion(new ApiVersion(1, 0))
+             .HasApiVersion(new ApiVersion(1, 1))
+             .Action(c => c.Delete(default(string), default(int)))      //This Means that Delete Operation is only applicable to Version 1.1
+             .MapToApiVersion(1, 1);
+         */
       });
       services.AddControllers();
       
